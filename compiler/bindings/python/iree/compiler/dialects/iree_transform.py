8294521ff360cce45ceb240cfffb8a05c42a9701
NaOHCC <NaOH@live.cn>
2025-03-12 23:39:17 +0800

    expose iree transform to python interface, ref: https://mlir.llvm.org/docs/Bindings/Python/#generating-_dialect_namespace_ops_genpy-wrapper-modules

---
+++ compiler/bindings/python/iree/compiler/dialects/iree_transform.py
@@ -0,0 +1,14 @@
+# Copyright 2024 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from ._iree_transform_ops_gen import *
+
+# from ._iree_common_extensions_ops_ops_gen import *
+# from ._iree_gpu_extensions_ops_ops_gen import *
+# from ._iree_llvmgpu_extensions_ops_ops_gen import *
+# from ._iree_common_extensions_ops_enum_gen import *
+# from ._iree_gpu_extensions_ops_enum_gen import *
+# from ._iree_llvmgpu_extensions_ops_enum_gen import *
